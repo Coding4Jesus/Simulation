dfc19136d034e0806e5cedff9f87ef1c5a17f629
Coding4Jesus <jesusmlgascon@gmail.com>
2021-04-14 09:49:04 -0500

    Readded wolves class but with comments this time

--- Simulation/src/sim/Simulation.java
+++ Simulation/src/sim/Simulation.java
@@ -3,10 +3,10 @@ public class Simulation {
 /* 
  * Jesus on wolves
  * Jack on rabbits
- * devin on the environment
+ * Devin on the environment
  */
 	public static void main(String[] args) {
-		Wolves wolves = new Wolves();
+		Wolves wolves = new Wolves();			//testing
 		Wolves wolves2 = new Wolves();
 		System.out.print(wolves.NumberW1);
 		
@@ -14,17 +14,17 @@ public class Simulation {
 	}
 	
 	public static class Wolves {
-		private static int NumberW1, Metabol2, StartF3, MaxF4, MaxAge5, ChancetoX6;
-		public Wolves() {
-			NumberW1 = 10;
-			Metabol2 = 10;
+		private static int NumberW1, Metabol2, StartF3, MaxF4, MaxAge5, ChancetoX6;		//the 6 variables I think we'll need
+		public Wolves() {																//numbered to keep them in mind more easily
+			NumberW1 = 10;	//initial values
+			Metabol2 = 10;	//just placeholder
 			StartF3 = 50;
 			MaxF4 = 150;
 			MaxAge5 = 50;
 			ChancetoX6 = 75;
 		}
-		public void Change1(int placehold) {
-			NumberW1 = placehold;
+		public void Change1(int placehold) { 
+			NumberW1 = placehold;				//they all change the initial values
 		}
 		public void Change2(int placehold) {
 			Metabol2 = placehold;
@@ -41,7 +41,7 @@ public class Simulation {
 		public void Change6(int placehold) {
 			ChancetoX6 = placehold;
 		}
-		public int return1() {
+		public int return1() {					//return said values
 			return(NumberW1);
 		}
 		public int return2() {
@@ -56,9 +56,9 @@ public class Simulation {
 		public int return5() {
 			return(MaxAge5);
 		}
-		public double return6() {
-			double a;
-			a=ChancetoX6/100;
+		public double return6() {				//ChancetoX6 is returned as a double since it 
+			double a;							//is easier to work with a fraction for its intended
+			a=ChancetoX6/100;					//purposes
 			return(a);
 		}
 	}
