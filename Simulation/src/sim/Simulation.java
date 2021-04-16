c6648584355b3b2384c493b771fae303141167c0
Jack <dixonja03@gmail.com>
2021-04-16 09:12:18 -0500

    testing to see if this will let me pull the most recent code

--- Simulation/src/sim/Simulation.java
+++ Simulation/src/sim/Simulation.java
@@ -1,10 +1,31 @@
+/*
+ * Jack Dixon
+ * Apr,12,2021
+ * make the code for rabbits in a simulation about wolves and rabbits :P
+ */
 package sim;
+
+import java.util.Random;
 
 public class Simulation {
 
 	public static void main(String[] args) {
+		Random r = new Random();
 		
-
+		int rabbits = 0;
+		int stage = 0;
+		int RabFood = 0;
+		int RabRepro = 0;
+		int RabMaxFood = 45;
+		int RabMaxAge = 25;
+		int RabMeta = 3;
+		int RabMinReFo = 40;
+		
+		// for starting with a random number of rabbits
+		rabbits = r.nextInt(24-18+1)+18;
+		
+		
+		
 	}
 
 }
